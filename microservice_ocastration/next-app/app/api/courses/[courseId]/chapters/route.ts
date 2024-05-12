8f987ebacc39764031347d10f346171d96715df2
Hansana <it21167850@my.sliit.lk>
2024-05-12 17:26:01 +0530

    update frontend part

--- microservice_ocastration/next-app/app/api/courses/[courseId]/chapters/route.ts
+++ microservice_ocastration/next-app/app/api/courses/[courseId]/chapters/route.ts
@@ -1,13 +1,16 @@
 import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import fetch from "node-fetch";
 
 export async function POST(
   req: Request,
-  { params }: {
+  {
+    params,
+  }: {
     params: {
-      courseId: string
-    }
+      courseId: string;
+    };
   }
 ) {
   try {
@@ -17,30 +20,41 @@ export async function POST(
     if (!userId) {
       return new NextResponse("Unauthorized", {
         status: 401,
-      })
+      });
     }
 
-    const courseOwner = await db.course.findUnique({
-      where: {
-        id: params.courseId,
-        userId: userId
+    // Check if the user owns the course
+    const courseOwnerResponse = await fetch(
+      "http://localhost:3004/api/is-course-owner",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.CLERK_API_KEY}`,
+        },
+        body: JSON.stringify({
+          userId,
+          courseId: params.courseId,
+        }),
       }
-    })
+    );
+
+    const courseOwner = await courseOwnerResponse.json();
 
     if (!courseOwner) {
       return new NextResponse("Unauthorized", {
         status: 401,
-      })
+      });
     }
 
     const lastChapter = await db.chapter.findFirst({
       where: {
-        courseId: params.courseId
+        courseId: params.courseId,
       },
       orderBy: {
-        position: "desc"
-      }
-    })
+        position: "desc",
+      },
+    });
 
     const newPosition = lastChapter ? lastChapter.position + 1 : 1;
 
@@ -48,13 +62,13 @@ export async function POST(
       data: {
         title,
         courseId: params.courseId,
-        position: newPosition
-      }
+        position: newPosition,
+      },
     });
 
-    return NextResponse.json(chapter)
+    return NextResponse.json(chapter);
   } catch (error) {
-    console.log("[CHAPTERS", error);
+    console.log("[CHAPTERS]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
