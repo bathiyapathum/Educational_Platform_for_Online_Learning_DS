894ec7c8765d2a1aa7cf297ac798ab24ff127043
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-05-15 14:12:04 +0530

    SMS added

--- microservice_ocastration/backend-course/server.js
+++ microservice_ocastration/backend-course/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const cors = require("cors");
+const twilio = require("twilio");
+const nodemailer = require("nodemailer");
+const { Vonage } = require('@vonage/server-sdk');
 const db = require("./lib/Db.js");
 
 const { getProgress } = require("./lib/GetProgress.js");
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   const timestamp = new Date()?.toISOString();
@@ -14,6 +18,89 @@ app.use((req, res, next) => {
   console.log(`[${timestamp}] | ${ip} | ${req?.method} ${req?.path}`);
   next();
 });
+
+
+const accountSid = "AC08b0fbcae9e75ad8d0d79024306a1c2a";
+const authToken = "e490009023f317063ff5c2691e371223";
+const client = twilio(accountSid, authToken);
+
+const sendSMS = async () => {
+  const msgOptions = {
+    from: "+15189630865", // Twilio phone number in E.164 format
+    to: "+94776048468", // Recipient's phone number in E.164 format
+    body: "You've taken a significant step forward by investing in this course for your future career. Congratulations on your decision!", // Include the message body
+  };
+
+  try {
+    const message = await client.messages.create(msgOptions);
+    console.log("SMS SID:", message.sid);
+    return "SMS sent successfully";
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to send SMS");
+  }
+};
+
+const vonage = new Vonage({
+  apiKey: "7a829464",
+  apiSecret: "fmCSyzn6XtYXZjtX"
+});
+const from = "Vonage APIs"
+const to = "94776048468"
+const text = "You've taken a significant step forward by investing in this course for your future career. Congratulations on your decision!"
+
+async function sendSMSS() {
+    await vonage.sms.send({to, from, text})
+        .then(resp => { console.log('Message sent successfully'); console.log(resp); })
+        .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
+}
+
+
+
+
+const sendEmail = async () => {
+  // Create a Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "dilanshanuka999@gmail.com", // Your Gmail address
+      pass: "huic jwfb ojzz seha", // Your Gmail password
+    },
+  });
+
+  // Define email options
+  const mailOptions = {
+    from: "dilanshanuka999@gmail.com",
+    to: "hellomotivation99@gmail.com",
+    subject: "Congratulations",
+    text: "You've taken a significant step forward by investing in this course for your future career. Congratulations on your decision!",
+  };
+
+  try {
+    // Send the email
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email ID:", info.messageId);
+    return "Email sent successfully";
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to send email");
+  }
+};
+
+
+app.post("/api/sendEmail", async(req,res) =>{
+  try {
+    console.log("hai dilan")
+    // Send SMS and Email
+    // const [smsMessage, emailInfo] = await Promise.all([sendSMS(), sendEmail()]);
+    const [smsMessage, emailInfo] = await Promise.all([ sendSMSS(),sendEmail()]);
+
+    res.status(200).json({ smsMessage, emailInfo });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 
 app.get("/api", async (req, res) => {
   const { userId, title, categoryId } = req.query;
--- microservice_ocastration/next-app/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ microservice_ocastration/next-app/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import toast from "react-hot-toast";
@@ -21,18 +21,36 @@ export const CourseEnrollButton = ({
   const onClick = async () => {
     try {
       const response = await axios.post(`/api/courses/${courseId}/checkout`);
-
+      triggerEmailAndMessage();
       window.location.assign(response.data.url);
     } catch (error) {
-      toast.error("Something went wrong")
+      toast.error("Something went wrong");
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
+
+  const triggerEmailAndMessage = async () => {
+    try {
+      console.log("hay dilan 1");
+      // Send request to trigger email and message functions
+      const res = await axios.post(`http://localhost:3001/api/sendEmail`);
+      toast.success("Email and SMS sent successfully");
+      console.log(res);
+      console.log("awa dilaaan");
+    } catch (error) {
+      toast.error("Failed to send email and SMS");
+    }
+  };
 
   return (
-    <Button size="sm" className="w-full md:w-auto" onClick={onClick} disabled={isLoading}>
+    <Button
+      size="sm"
+      className="w-full md:w-auto"
+      onClick={onClick}
+      disabled={isLoading}
+    >
       Enroll for {formatPrice(price)}
     </Button>
-  )
-}
+  );
+};
